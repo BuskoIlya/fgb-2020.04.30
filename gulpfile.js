6c379ae3c2212c6ce28c58b972e18e8a9c097e88
BuskoIlya <trassae95st@mail.ru>
2020-05-02 10:07:16 +0300

    [Gulp] PHP Server Here works, but 'gulp' don't

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,38 @@ const less = require('gulp-less');
 const mincss = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const minjs = require('gulp-minify');
+const browsersync = require('browser-sync').create();
 
-gulp.task('min-css', async function(done) {
+gulp.task('min_css', async function(done) {
   gulp.src('app/css/**/*.less')
   .pipe(less())
   .pipe(mincss())
   .pipe(rename({
     suffix: '_min'
   }))
-  .pipe(gulp.dest('public/css'));
+  .pipe(gulp.dest('public/css'))
+  .pipe(browsersync.stream());
 });
 
-gulp.task('min-js', async function(done) {
+gulp.task('min_js', async function(done) {
   gulp.src('app/js/**/*.js')
   .pipe(minjs())
-  .pipe(gulp.dest('public/js'));
+  .pipe(gulp.dest('public/js'))
+  .pipe(browsersync.stream());
 });
 
-gulp.task('default', gulp.series('min-css', 'min-js'));
+gulp.task('watch_all', function() {
+  gulp.watch("app/css/*.scss", gulp.series('min_css'));
+  gulp.watch("app/js/*.js", gulp.series('min_js'));
+});
+
+gulp.task('sync', function() {
+  browsersync.init({
+    server: {
+      baseDir: "public/"
+    }
+  });
+  gulp.watch("public/*.html").on('change', browsersync.reload);
+});
+
+gulp.task('default', gulp.parallel('sync', 'watch_all'));
--- public/pages/blocks/head.php
+++ public/pages/blocks/head.php
@@ -5,7 +5,7 @@
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 
-<link rel="stylesheet/less" href="css/constants.less">
-<link rel="stylesheet/less" href="css/index.less">
+<link rel="stylesheet/less" href="css/constants_min.css">
+<link rel="stylesheet/less" href="css/index_min.css">
 
 <script type="text/javascript" src="libs/less.min.js"></script>
